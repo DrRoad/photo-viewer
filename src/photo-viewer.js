ec3debebece820be117e234991285cd1ddc9524d
Jairaj Sethi <j@jairaj.org>
2013-01-16 18:40:33 -0500

    init

---
+++ src/photo-viewer.js
@@ -0,0 +1,13 @@
+var PhotoViewer = function (Zepto, jQuery, App) {
+	//TODO: init
+
+	return PhotoViewer;
+
+
+	function PhotoViewer () {
+		//TODO
+	}
+
+
+	//TODO: helpers
+}(window.Zepto, window.jQuery, App);
---
+++ src/slide-viewer.js
@@ -0,0 +1,13 @@
+var SlideViewer = function (Zepto, jQuery, App) {
+	//TODO: init
+
+	return SlideViewer;
+
+
+	function SlideViewer () {
+		//TODO
+	}
+
+
+	//TODO: helpers
+}(window.Zepto, window.jQuery, App);
