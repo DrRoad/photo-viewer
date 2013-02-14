e7bbbdaa27e0ee80dc21d6031ab05bddee42c0dd
crazy2be <crazy1be@gmail.com>
2013-02-14 16:57:00 -0500

    Cleaup. Moved Zoomable into own file, deleted some crazy experimental code that wasn't being used, made loadingElm a opt argument rather than a api funtion (much better api really, you almost never want to change your loading element while between images).

--- src/photo-viewer.js
+++ src/photo-viewer.js
@@ -1,271 +1,4 @@
 var PhotoViewer = (function (Zepto, jQuery, App) {
-	function TranformStacker(elm) {
-		var self = this;
-		var x = 0;
-		var y = 0;
-		var scale = 1;
-
-		var ax = 0;
-		var ay = 0;
-		var ascale = 1;
-		var astart = 0;
-		var at = 0;
-
-		function reapply() {
-			var s = elm.style;
-			var transform = 'translate3d(' + round(x * scale, 2) + 'px,' + round(y * scale, 2) + 'px,0px) scale(' + round(scale, 2) + ',' + round(scale, 2) + ') ';
-			s.webkitTransform = '';
-			s.webkitTransition = 'transform 0ms';
-
-		}
-
-		self.addTranslate = function (dx, dy, t) {
-			if (t === 0 || t === undefined) {
-				x += dx;
-				y += dy;
-				reapply();
-			} else {
-
-			}
-		}
-		// t: Time, [0...d]
-		// b: Start value
-		// c: Change in value
-		// d: Total duration
-		function easeInOutCubic(t, b, c, d) {
-			t /= d/2;
-			if (t < 1) return c/2*t*t*t + b;
-			t -= 2;
-			return c/2*(t*t*t + 2) + b;
-		};
-	}
-	function Zoomable(viewport, element, parent) {
-		var self = this;
-		var prevTouchEnd = 0;
-		var x, y, scale;
-
-		self.reset = function () {
-			x = 0;
-			y = 0;
-			scale = 1;
-			prevTouchEnd = 0;
-			setTransform(0);
-			return self;
-		}
-
-		self.destroy = function () {
-			touchy.stop();
-			return self;
-		}
-
-		function dist(p1, p2) {
-			p1 = p1.x ? p1 : p1.lastPoint;
-			p2 = p2.x ? p2 : p2.lastPoint;
-			return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-		}
-		function center(p1, p2) {
-			return {
-				x: (p1.x + p2.x) / 2,
-				y: (p1.y + p2.y) / 2,
-			};
-		}
-		function abs(n) {
-			return Math.abs(n);
-		}
-		function roundObj(o) {
-			var newO = {};
-			for (k in o) {
-				if (typeof o[k] == 'number') {
-					newO[k] = round(o[k], 2);
-				} else {
-					newO[k] = o[k];
-				}
-			}
-			return JSON.stringify(newO) + ' ';
-
-		}
-		function setTransform(t) {
-			var r = round;
-			var tx = r(x * scale, 2);
-			var ty = r(y * scale, 2);
-			var ts = r(scale, 2);
-
-			var tr = 'translateX(' + tx + 'px) ' +
-				'translateY(' + ty + 'px) ' +
-				'scale(' + ts + ',' + ts + ')';
-			var tp = t === 0 ? 'none' : 'all';
-			var td = r(t, 0) + 'ms';
-
-			var s = element.style;
-			s.webkitTransitionProperty = tp;
-			s.webkitTransitionDuration = td;
-			s.webkitTransform = tr;
-		}
-		function viewHalfX() {
-			return viewport.offsetWidth / 2;
-		}
-		function viewHalfY() {
-			return viewport.offsetHeight / 2;
-		}
-		function findMaxX() {
-			var maxX = element.offsetWidth / 2 - viewHalfX() / scale;
-			if (maxX < 0) return 0;
-			else return maxX;
-		}
-		function findMaxY() {
-			var maxY = element.offsetHeight / 2 - viewHalfY() / scale;
-			if (maxY < 0) return 0;
-			else return maxY;
-		}
-		function boundXandY() {
-			var maxX = findMaxX();
-			if (Math.abs(x) > maxX) {
-				x = x > 0 ? maxX : -maxX;
-			}
-			var maxY = findMaxY();
-			if (Math.abs(y) > maxY) {
-				y = y > 0 ? maxY : -maxY;
-			}
-		}
-		// Converts an abitrary point in screen
-		// coordinates to the corresponding point
-		// in image coordinates, given the transforms
-		// and scaling currently in place.
-		//
-		//    screen coordinates        image coordinates
-		//      +--------(+viewW)            (+maxY)
-		//      |                               |
-		//      |                   (+maxX)-----+------(-maxX)
-		//      |                               |
-		//    (+viewH)                       (-maxY)
-		//
-		// Notice X and Y are flipped, and the origin
-		// has moved from the top-left corner to the
-		// center.
-		function sc2ic(sc) {
-			return {
-				x: x + (viewHalfX() - sc.x) / scale,
-				y: y + (viewHalfY() - sc.y) / scale,
-			}
-		}
-		var touchy = Touchy(viewport, {
-		one: function (hand, finger) {
-			var prevX = finger.lastPoint.x;
-			var prevY = finger.lastPoint.y;
-
-			var maxX = findMaxX();
-			if (Math.abs(x) >= maxX) {
-				parent.enable();
-			}
-			boundXandY();
-
-			finger.on('move', function (point) {
-				prevTouchEnd = 0;
-				if (scale <= 1) return;
-
-				var dx = (point.x - prevX) / scale;
-				var dy = (point.y - prevY) / scale;
-				x += dx;
-				y += dy;
-
-				prevX = point.x;
-				prevY = point.y;
-
-				var maxX = findMaxX();
-				if (Math.abs(x) <= maxX) {
-					parent.disable();
-				} else if (parent.moving()) {
-					return;
-				}
-
-				setTransform(0);
-			});
-
-			finger.on('end', function (point) {
-				if (parent.moving()) {
-					boundXandY()
-					setTransform(300);
-					return;
-				}
-
-				var t = Date.now();
-				var diff = t - prevTouchEnd;
-				if (diff < 200) {
-					if (scale <= 1) {
-						scale = 2;
-						var ic = sc2ic(finger.lastPoint);
-						x = ic.x;
-						y = ic.y;
-						boundXandY();
-						setTransform(500);
-					} else {
-						scale = 1;
-						x = 0;
-						y = 0;
-						setTransform(500);
-					}
-					prevTouchEnd = 0;
-					return;
-				}
-				prevTouchEnd = t;
-
-				boundXandY();
-				setTransform(500);
-			});
-		},
-		two: function (hand, finger1, finger2) {
-			prevTouchEnd = 0;
-			if (parent.moving()) return;
-			parent.disable();
-
-			var p1 = finger1.lastPoint;
-			var p2 = finger2.lastPoint;
-
-			var prevDist = dist(p1, p2);
-			var startCenter = sc2ic(center(p1, p2));
-
-			hand.on('move', function (points) {
-				var p1 = points[0];
-				var p2 = points[1];
-
-				var newDist = dist(p1, p2);
-				scale *= newDist / prevDist;
-				prevDist = newDist;
-
-				// We try and keep the same center, in
-				// image coordinates, for the pinch
-				// as the user had when they started.
-				// This allows two finger panning, and a
-				// pleasent "zooms to your fingers"
-				// feeling.
-				var newCenter = sc2ic(center(p1, p2));
-				x += startCenter.x - newCenter.x;
-				y += startCenter.y - newCenter.y;
-
-				setTransform(0);
-			});
-
-			hand.on('end', function () {
-				var minZoom = 1;
-				var maxZoom = 4;
-				if (scale <= 1) {
-					parent.enable();
-				}
-				if (scale < minZoom) {
-					scale = minZoom;
-					x = 0;
-					y = 0;
-				}
-				if (scale > maxZoom) {
-					scale = maxZoom;
-				}
-				boundXandY();
-				setTransform(300);
-			});
-		},
-		});
-		self.reset();
-	}
 	var loaderImg = [
 		"data:image/gif;base64,",
 		"R0lGODlhEAAQAPIAAAAAAP///zw8PLy8vP///5ycnHx8fGxsbCH+GkNyZWF0ZWQgd2l0aCBhamF4",
@@ -281,9 +14,20 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 		"xTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAAKAAcALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdce",
 		"CAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==",
 	].join('');
+
+	var defaultLoadingElm = (function () {
+		var elm = document.createElement('div');
+		var s = elm.style;
+		s.width = '100%';
+		s.height = '100%';
+		s.background = 'url(' + loaderImg + ') no-repeat center center';
+		return elm;
+	}());
+
 	var defaultOpts = {
 		automaticTitles: true,
 		autoHideTitle: true,
+		loadingElm: defaultLoadingElm,
 	};
 	return PhotoViewer;
 
@@ -343,32 +87,30 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 		wrapper.style.width = '100%';
 		wrapper.style.height = '100%';
 
-		var loaderElm = document.createElement('div');
-		loaderElm.style.width = '100%';
-		loaderElm.style.height = '100%';
-		loaderElm.style.background = 'url(' + loaderImg + ') no-repeat center center';
-
 		// If you want the loader element to be custom skinned for your application,
 		// you can pass in an element here to be used for a placeholder while your
 		// images are loading.
-		self.setLoader = function (newLoaderElm) {
-			loaderElm = newLoaderElm;
+		self.setLoader = function (newLoadingElm) {
+			console.warn("PhotoViewer.setLoader() is depreciated! Use opts.loadingElm instead.");
+			loadingElm = newLoadingElm;
 			if (slideviewer) {
 				slideviewer.invalidate();
 			} else {
-				replaceChildren(content, loaderElm);
+				replaceChildren(content, loadingElm);
 			}
 			return self;
 		}
 		self.on = dispatcher.on;
 		self.off = dispatcher.off;
 
-		replaceChildren(content, loaderElm);
 
 		opts = opts || {};
 		for (var o in defaultOpts) {
 			opts[o] = opts[o] === undefined ? defaultOpts[o] : opts[o];
 		}
+
+		var loadingElm = opts.loadingElm;
+		replaceChildren(content, loadingElm);
 
 		if (opts.autoHideTitle) {
 			Clickable(wrapper);
@@ -396,7 +138,15 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 		return;
 
 		function appLayout() {
-			dispatcher.fire('layout');
+			if (!slideviewer) return;
+			slideviewer.refreshSize();
+			slideviewer.eachMaster(function (elm) {
+				var wrap = elm.querySelector('div');
+				var img = elm.querySelector('img');
+				if (wrap && img) {
+					centerImage(wrap, img);
+				}
+			});
 		}
 
 		function appBack() {
@@ -478,7 +228,6 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 
 			var zoomable;
 			slideviewer = new SlideViewer(wrapper);
-			setSource(urls);
 			slideviewer.on('flip', function (page, elm) {
 				updateTitle(page, urls.length);
 
@@ -490,6 +239,7 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 				dispatcher.fire('flip', page);
 			});
 			slideviewer.refreshSize();
+			setSource(urls);
 			slideviewer.setPage(index);
 
 			if (App.platform == 'ios') {
@@ -507,17 +257,6 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 			cs.opacity = "0";
 			cs.visibility = "hidden";
 			page.appendChild(topbarCover);
-
-			dispatcher.on('layout', function () {
-				slideviewer.refreshSize();
-				slideviewer.eachMaster(function (elm) {
-					var wrap = elm.querySelector('div');
-					var img = elm.querySelector('img');
-					if (wrap && img) {
-						centerImage(wrap, img);
-					}
-				});
-			});
 		}
 
 		function centerImage(wrap, img) {
@@ -577,7 +316,7 @@ var PhotoViewer = (function (Zepto, jQuery, App) {
 				ws.height = '100%';
 				ws.overflow = 'hidden';
 
-				var elm = loaderElm.cloneNode(true /* deep copy */);
+				var elm = loadingElm.cloneNode(true /* deep copy */);
 				wrap.appendChild(elm);
 
 				var img = document.createElement('img');
